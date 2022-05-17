ee71439eca5b1242a09ab87377582653a6c0f0a3
moeendehqan <moeen.dehqan@gmail.com>
2022-05-17 13:19:52 +0430

    h9

--- src/componet/desk/view/upload.js
+++ src/componet/desk/view/upload.js
@@ -2,10 +2,24 @@
 const Upload = (props) =>{
     if(props.viw==='upload'){
         return(
-            <p>upload</p>
+            <div className="upload">
+                <img src={require('../../../img/icon/upload.png')} alt='upload icon'></img>
+                <form>
+                    <label>
+                        <span> فایل  معاملات</span>
+                        <input type='file'></input>
+                    </label>
+                    <label>
+                    <span> فایل  رجیستر</span>
+                        <input type='file'></input>
+                    </label>
+                    <button className='uplbtn'>بارگذاری</button>
+                    
+                </form>
+
+            </div>
         )
     }
-
 }
 
 export default Upload
