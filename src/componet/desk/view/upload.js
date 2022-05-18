c690d31843e3bdea2a4a55f1dd029e53c08468ac
moeendehqan <moeen.dehqan@gmail.com>
2022-05-18 11:14:55 +0430

    qqqqww554

--- src/componet/desk/view/upload.js
+++ src/componet/desk/view/upload.js
@@ -57,7 +57,6 @@ const Upload = (props) =>{
                 const formData = new FormData();
                 formData.append('Trade',filetrade)
                 formData.append('Register',fileregister)
-                formData.append('user',props.user)
                 axios({
                     method: 'post',
                     url: "http://localhost:5000/api/getfile",
