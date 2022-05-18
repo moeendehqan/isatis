e8abb4141138fa3faca85e82e8a8cd92ebd9c4a9
moeendehqan <moeen.dehqan@gmail.com>
2022-05-18 09:08:50 +0430

    q3

--- src/componet/desk/view/upload.js
+++ src/componet/desk/view/upload.js
@@ -1,40 +1,55 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import axios from 'axios'
 
 
 const Upload = (props) =>{
+
+    const filetradeRef = useRef(null)
     const [filetrade, setFiletrade] = useState(null)
-    const [isFaileTrade, setIsFaileTrade] = useState(false)
+    const [isFileTrade, setIsFileTrade] = useState(false)
     const handlefiletrade = (e)=>{
         setFiletrade(e.target.files[0])
-        setIsFaileTrade(true)
+        setIsFileTrade(true)
     }
 
+    const fileregisterRef = useRef(null)
     const [fileregister, setFileregister] = useState(null)
-    const [isFaileRegister, setIsFaileRegister] = useState(false)
+    const [isFileRegister, setIsFileRegister] = useState(false)
     const handlefileregister = (e) =>{
         setFileregister(e.target.file[0])
-        setIsFaileRegister(true)
+        setIsFileRegister(true)
     }
 
-    if(isFaileTrade===true){
-        console.log(filetrade.name)
+
+    const [msg, setMsg] = useState('')
+
+    const typeAllowe = ['xlsx']
+    if(isFileTrade){
+        const typetrade = filetrade.name.split('.')[filetrade.name.split('.').length -1]
+        console.log(typetrade)
     }
 
     const handleFormUpload = (e) =>{
-        e.preventDefault();
-        const formData = new FormData();
-        formData.append('Trade',filetrade)
-        formData.append('Register',fileregister)
-        axios({
-            method: 'post',
-            url: "http://localhost:5000/api/getfile",
-            data: formData,
-        }).then((response)=>{
-            console.log(response.data.msg)
-        }).catch((response)=>{
-            console.log(response)
-        })
+        if(isFileTrade){
+            e.preventDefault();
+            const formData = new FormData();
+            formData.append('Trade',filetrade)
+            formData.append('Register',fileregister)
+            axios({
+                method: 'post',
+                url: "http://localhost:5000/api/getfile",
+                data: formData,
+            }).then((response)=>{
+                console.log(response.data.msg)
+            }).catch((response)=>{
+                console.log(response)
+            })
+        }else{
+            setMsg('فایل معاملات را وارد کنید')
+            filetradeRef.current.focuse()
+        }
+        
+
     }
 
 
@@ -45,18 +60,11 @@ const Upload = (props) =>{
                 <form onSubmit={(e)=>handleFormUpload(e)}>
                     <label>
                         <span> فایل  معاملات</span>
-                        <input type='file' onChange={(e)=>handlefiletrade(e)}></input>
-                            {isFaileTrade?(
-                                <div className='dtl'>
-                                    <p>نوع:</p>
-                                    <p>{filetrade.name}</p>
-                                </div>
-                            ):(<p></p>)}
-
+                        <input type='file' onChange={(e)=>handlefiletrade(e)} ref={filetradeRef}></input>
                     </label>
                     <label>
                     <span> فایل  رجیستر</span>
-                        <input type='file' onChange={(e)=>handlefileregister(e)}></input>
+                        <input type='file' onChange={(e)=>handlefileregister(e)} ref={fileregisterRef}></input>
                     </label>
                     <button className='uplbtn' type='submit'>بارگذاری</button>
                 </form>
