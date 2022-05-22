779486d14f0aaac86e08af857d795aaa64b92014
moeendehqan <moeen.dehqan@gmail.com>
2022-05-22 11:01:08 +0430

    qq7935

---
+++ src/componet/desk/view/newtraders.js
@@ -0,0 +1,16 @@
+
+const Newtraders = (props) =>{
+    
+    const user = props.user
+
+    if(props.viw==='newtraders'){
+        return(
+            <div>
+                جدید الورود
+            </div>
+        )
+    }
+
+}
+
+export default Toptraders
