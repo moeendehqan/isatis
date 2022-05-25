19e1068806fc53a66d2e3985ec0da017c732c4ad
moeendehqan <moeen.dehqan@gmail.com>
2022-05-25 13:44:45 +0430

    cxcx

--- src/componet/desk/menu/menuright.js
+++ src/componet/desk/menu/menuright.js
@@ -1,29 +1,31 @@
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCheckSquare, faCoffee } from '@fortawesome/fontawesome-free-solid'
+
+
 const Menu = (props) =>{
-    const menuright = props.menuright
     
 
-    if(menuright){
         return(
             <div className="menur">
-                <button className="btm" onClick={(e)=>props.handleViw('upload')}>
-                    بارگذاری فایل
-                    </button>
-                <button className="btm" onClick={(e)=>props.handleViw('Toptraders')}>
-                    معامله گران برتر روز
-                    </button>
-                <button className="btm" onClick={(e)=>props.handleViw('newtraders')}>
-                    معامله گران جدیدالورود
-                    </button>
-                <button className="btm" onClick={(e)=>props.handleViw('istgah')}>
-                    ایستگاه های معاملاتی
-                    </button>
+                <div id='upbtm' className="btm" onClick={(e)=>props.handleViw('upload')}>
+                    <p> <FontAwesomeIcon icon="fa-solid fa-upload" /> </p>
+                </div>
+                <div id='toptr' className="btm" onClick={(e)=>props.handleViw('Toptraders')}>
+                    <p> <FontAwesomeIcon icon="fa-solid fa-user-tie" /> </p>
+                </div>
+                <div id='newtr' className="btm" onClick={(e)=>props.handleViw('newtraders')}>
+                    <p> <FontAwesomeIcon icon="fa-solid fa-user" /> </p>
+                </div>
+                <div id='istgh' className="btm" onClick={(e)=>props.handleViw('istgah')}>
+                    <p> <FontAwesomeIcon icon="fa-solid fa-building" /> </p>
+                </div>
 
             </div>
 
             
         )
-    }
+
 }
 
 export default Menu
